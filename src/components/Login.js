3a461a536407ee89ca84543cf9b8eff452b8d36b
LakmaliPalihakkara <200512551@student.georgianc.on.ca>
2022-11-16 23:04:34 -0500

    Login and Register changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { auth,logInWithEmailAndPassword } from "../firebase";
+import { auth, logInWithEmailAndPassword } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./Login.css";
+import img from '../images/login.jpg'
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -10,6 +11,15 @@ function Login() {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
 
+
+  //Error validation
+  const login = () => {
+    if (!email) alert("Please enter your email");
+    else if (!password) alert("Please enter your password");
+    else logInWithEmailAndPassword(email, password)
+  };
+
+  // Login success
   useEffect(() => {
     if (loading) {
 
@@ -20,30 +30,43 @@ function Login() {
 
   return (
     <div className="login">
-      <div className="login__container">
-        <input
-          type="text"
-          className="login__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="login__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button
-          className="login__btn"
-          onClick={() => logInWithEmailAndPassword(email, password)}
-        >
-          Login
-        </button>
-        <div>
-          Don't have an account? <Link to="/register">Register</Link> now.
+
+      <div class="container">
+        <img src={img} class="login-img" alt='logo' />
+
+        <div className="login__container">
+
+        <h1 id="title">Login</h1>
+
+          {/* Email text input */}
+          <input
+            type="text"
+            className="login__textBox"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail Address"
+          />
+
+          {/* Password text input */}
+          <input
+            type="password"
+            className="login__textBox"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+
+          {/* Register button */}
+          <button
+            className="login__btn"
+            onClick={login}>
+            Login
+          </button>
+          <div>
+            Don't have an account? <Link to="/register">Register</Link> now.
+          </div>
         </div>
+
       </div>
     </div>
   );
